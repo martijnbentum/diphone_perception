14f11eb746e3bac5d7049fd9ed2391b1aed224e5
martijnbentum <bentummartijn@gmail.com>
2025-08-01 17:53:39 +0200

    added locations

--- locations.py
+++ locations.py
@@ -14,3 +14,13 @@ matrix_plots = base / 'confusion_matrix_plots'
 gate_timestamps = base / 'tone_timestamps.csv'
 
 model_responses = Path('model_responses')
+
+cgn = Path('/vol/bigdata/corpora2/CGN2/')
+
+st_phonetics= Path('/vol/mlusers/mbentum/st_phonetics/')
+news_books_cgn_sentences = st_phonetics / 'news_books_sentences_zs.tsv'
+
+csv_logs = Path('csv_logs')
+selected_phonemes = csv_logs / 'success.csv'
+selected_sentences = Path('selected_sentences.json')
+
