9c7387184ff95b523228b34258109df156c74699
martijnbentum <bentummartijn@gmail.com>
2025-09-19 18:59:50 +0200

    cv json file creation; mls work in progress

--- pretraining_metadata.py
+++ pretraining_metadata.py
@@ -23,6 +23,20 @@ def make_or_load_cgn_phrases_json(phrases = None, speakers = None,
     print(f'Skipped {len(error)} phrases with unknown phonemes')
     return outputs 
 
+def make_or_load_common_voice_phrases_json(common_voice_words= None, 
+    overwrite = False):
+    if locations.common_voice_phrases_json.exists() and not overwrite:
+        print('loading existing common voice phrases json')
+        with locations.common_voice_phrases_json.open('r') as f:
+            outputs = json.load(f)
+        return outputs
+    print('making new common voice phrases json')
+    outputs = clean_common_voice_phrases(common_voice_words)
+    with locations.common_voice_phrases_json.open('w') as f:
+        json.dump(outputs, f, indent=4)
+    print(f'Wrote {len(outputs)} phrases to {locations.common_voice_phrases_json}')
+    return outputs
+
 
 def make_cgn_phrase_name(speaker_id, file_id, start_time, end_time):
     m = f'{speaker_id}_{file_id}_{start_time:.3f}-{end_time:.3f}'
@@ -111,16 +125,18 @@ def load_common_voice_words():
     output = [x.split('\t') for x in t[1:] if x]
     return output
 
-def clean_common_voice_phrases(common_voice_words= None):
+def clean_common_voice_phrases(common_voice_words= None, check_manifest= True):
     if not common_voice_words:
         common_voice_words = load_common_voice_words()
     d = cgn_phonemes.Ipa().ipa_to_simple_ipa_dict_mauser
     cv_phrases = _common_voice_words_to_phrases(common_voice_words)
+    manifest = load_manifest()
     outputs = []
-    for filename, words in cv_phrases.items():
+    for filename, words in progressbar(cv_phrases.items()):
         speaker = words[0][5]
         speaker_id, gender, age = _extract_speakerid_gender_age(speaker)
         ipa_phrase, ort_phrase = _common_voice_words_to_ipa_ort_phrase(words, d)
+        in_manifest = filename in manifest if check_manifest else None
         output = {
             'ipa': ipa_phrase,
             'orthographic': ort_phrase,
@@ -130,12 +146,14 @@ def clean_common_voice_phrases(common_voice_words= None):
             'audio_filename': filename,
             'corpus': 'common voice',
             'language': 'Netherlandic Dutch',
+            'in_manifest': in_manifest,
             }
         outputs.append(output)
     return outputs
 
 def _common_voice_words_to_phrases(common_voice_words= None):
     if not common_voice_words:
+        print('loading common voice words')
         common_voice_words = load_common_voice_words()
     d = {}
     for x in common_voice_words:
@@ -144,6 +162,12 @@ def _common_voice_words_to_phrases(common_voice_words= None):
             d[filename] = []
         d[filename].append(x)
     return d
+
+def _mls_words_to_phrases(mls_words= None):
+    if not mls_words:
+        print('loading mls words')
+        mls_words = load_mls_words()
+    return _common_voice_words_to_phrases(mls_words)
 
 def _common_voice_words_to_ipa_ort_phrase(words, ipa_dict):
     ipa_phrase = []
@@ -155,11 +179,69 @@ def _common_voice_words_to_ipa_ort_phrase(words, ipa_dict):
         ort_phrase.append(word[4])
     return '  '.join(ipa_phrase), ' '.join(ort_phrase)
 
+def _mls_words_to_ipa_ort_phrase(words, ipa_dict):
+    ipa_phrase = []
+    ort_phrase = []
+    for word in words:
+        word_ipa_mauser = word[6].split(' ')
+        word_ipa = ' '.join([ipa_dict[x] for x in word_ipa_mauser])
+        ipa_phrase.append(word_ipa)
+        ort_phrase.append(word[5])
+    return '  '.join(ipa_phrase), ' '.join(ort_phrase)
+
 def _extract_speakerid_gender_age(s):
     if not s: return None, None, None
-    speaker_id, gender, age= s.split('_')
+    speaker_id, gender, age= None, None, None
+    o =  s.split('_')
+    if len(o) > 0: speaker_id = o[0]
+    if len(o) > 1: gender = o[1]
+    if len(o) > 2: age = o[2]
     if not speaker_id: speaker_id = None
     if not gender: gender = None
     if not age: age = None
     return speaker_id, gender, age
+
+def load_mls_phonemes():
+    with locations.mls_phonemes.open('r') as f:
+        t = f.read().split('\n')
+    output = [x.split('\t') for x in t[1:] if x]
+    return output
+
+def load_mls_words():
+    with locations.mls_words.open('r') as f:
+        t = f.read().split('\n')
+    output = [x.split('\t') for x in t[1:] if x]
+    return output
+
+
+def clean_mls_phrases(mls_words= None, check_manifest= True):
+    if not mls_words:
+        mls_words = load_mls_words()
+    d = cgn_phonemes.Ipa().ipa_to_simple_ipa_dict_mauser
+    mls_phrases = _mls_words_to_phrases(common_voice_words)
+    manifest = load_manifest()
+    outputs = []
+    error = []
+    for filename, words in progressbar(cv_phrases.items()):
+        if len(words) == 0:
+            error.append(filename)
+            continue
+        speaker = words[0][-2]
+        speaker_id, gender, age = _extract_speakerid_gender_age(speaker)
+        ipa_phrase, ort_phrase = _mls_words_to_ipa_ort_phrase(words, d)
+        in_manifest = filename in manifest if check_manifest else None
+        output = {
+            'ipa': ipa_phrase,
+            'orthographic': ort_phrase,
+            'speaker_id': speaker_id,
+            'gender': gender,
+            'age': age,
+            'audio_filename': filename,
+            'corpus': 'mls',
+            'language': 'Netherlandic Dutch',
+            'in_manifest': in_manifest,
+            }
+        outputs.append(output)
+    print(f'Skipped {len(error)} empty phrases', ' '.join(error))
+    return outputs
     
